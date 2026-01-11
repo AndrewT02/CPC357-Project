b755c90ef747f42fe82379b15387bcdc28e3af73
AndrewT02 <teeandrew86@gmail.com>
2026-01-11 14:52:58 +0800

    Remove secrets.h and .env

--- firmware/src/secrets.h
+++
@@ -1,8 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-const char* WIFI_SSID = "Galaxy Note10 Lite2a71";
-const char* WIFI_PASSWORD = "noob1234";
-const char* MQTT_SERVER_IP = "34.171.38.56"; //Change to GCP VM IP
-
-#endif
